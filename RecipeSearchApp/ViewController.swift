fbbad4eacdbad644cd89f95f1ee1ca9506844721
Passant Abdelatif <passantabdelatif@gmail.com>
2022-11-02 03:29:59 +0200

    init commit
    - create new project with RecipeSearchApp
    - init pod file to add cocoapods libs

---
+++ RecipeSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeSearchApp
+//
+//  Created by Passant Abdelatif on 02/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
